06841460afabf1cd2b4c6cc1f329021a6893e235
Oleg Matviichuk <ma7hway@gmail.com>
2021-04-17 21:56:49 +0300

    Added 'Search' task implementation

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from markdown import markdown
-from django.http import Http404
+from django import forms
 
 from . import util
 
@@ -18,3 +20,12 @@ def entry(request, title):
         })
     else:
         return render(request, "encyclopedia/404.html", status = 404)
+
+def search(request):
+    title = request.GET['q']
+    if util.get_entry(title):
+        return HttpResponseRedirect(reverse("entry", args=[title]))
+    else:
+        return render(request, "encyclopedia/search.html", {
+            "entries": [entry for entry in util.list_entries() if title in entry]
+        })
